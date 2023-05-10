e14463b7ec79aaa55e469d1a88d8e66487dd6082
Andreas Wulf <awulf@pacmeb.com>
2023-05-11 00:04:14 +0930

    Fix zone setpoint restrictions

--- src/Actron485.cpp
+++ src/Actron485.cpp
@@ -45,7 +45,10 @@ namespace Actron485 {
         zoneMessage[zindex(zone)].type = ZoneMessageType::Normal;
         zoneMessage[zindex(zone)].zone = zone;
         zoneMessage[zindex(zone)].temperature = zoneTemperature[zindex(zone)];
-        zoneMessage[zindex(zone)].setpoint = max(min(zoneSetpoint[zindex(zone)], masterToZoneMessage[zindex(zone)].maxSetpoint), masterToZoneMessage[zindex(zone)].minSetpoint);
+
+        // Enforce, and set based on setpoint range limit
+        zoneSetpoint[zindex(zone)] = max(min(zoneSetpoint[zindex(zone)], masterToZoneMessage[zindex(zone)].maxSetpoint), masterToZoneMessage[zindex(zone)].minSetpoint);
+        zoneMessage[zindex(zone)].setpoint = zoneSetpoint[zindex(zone)];
         
         uint8_t data[zoneMessage[zindex(zone)].messageLength];
         zoneMessage[zindex(zone)].generate(data);
